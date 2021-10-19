7dbc5549ab72b8637be283fc38453ebcdadee288
cjop1 <91502016+cjop1@users.noreply.github.com>
2021-10-19 10:09:07 -0500

    Primer commit

--- src/main/java/com/retos/rentacar/RentacarApplication.java
+++ src/main/java/com/retos/rentacar/RentacarApplication.java
@@ -2,7 +2,9 @@ package com.retos.rentacar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.retos.rentacar.modelo"})
 @SpringBootApplication
 public class RentacarApplication {
 
